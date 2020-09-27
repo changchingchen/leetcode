7243828b8cfded472042f1e3c065fc20d7034ecd
changchingchen <tim.changching.chen@gmail.com>
2020-09-27 14:28:05 +0800

    Leetcode 1431

---
+++ 1431_KidsWithTheGreatestNumberOfCandies/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        max_candies = max(candies)
+        return [(c + extraCandies) >= max_candies for c in candies]
+
+# Time: O(N)
+# Space: O(1)
